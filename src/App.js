4f0a2c08c89d2fd46c3faa8931f658c5cd40c307
Alex J Garcia <alexshuu@gmail.com>
2022-08-02 19:46:32 -0400

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import { useState } from 'react';
+import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Playground from './components/Playground/Playground';
-import lessons from './lessons.json'
+import lessons from './lessons.json';
 
 function App() {
   const [lesson, setLesson] = useState(lessons[0]);
 
   return (
     <div className='App'>
-      <Menu links={lesson} />
-      <Playground />
+      <Header />
+      <main>
+        <Menu links={lesson} />
+        <Playground />
+      </main>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className='Header'>
+      <button>🇪🇸 Spanish</button>
+      <button>🌙</button>
+    </header>
+  );
+}
+
+export default Header;
